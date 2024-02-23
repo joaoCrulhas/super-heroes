05b456f68d571be1be2f3660c0f77eba4919492f
Joao Crulhas <joao.crulhas@gmail.com>
2024-02-23 09:47:03 +0100

    Create validations

--- src/infra/encrypter/service/service.go
+++ src/infra/encrypter/service/service.go
@@ -1,16 +1,36 @@
 package encrypter
 
-import custom_errors "github.com/joaoCrulhas/omnevo-super-heroes/src/error"
+import (
+	"unicode"
 
-type Service struct{}
+	custom_errors "github.com/joaoCrulhas/omnevo-super-heroes/src/error"
+)
 
-func NewEncryptService() Service {
-	return Service{}
+type Service struct {
+	key uint32
 }
 
-func (e *Service) Encrypt(value string) (string, error) {
+func NewEncryptService(key uint32) Service {
+	return Service{
+		key: key,
+	}
+}
+
+func (service *Service) hasInvalidChars(value string) bool {
+	for _, r := range value {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func (service *Service) Encrypt(value string) (string, error) {
 	if value == "" {
 		return "", custom_errors.EmptyString()
+	}
+	if !service.hasInvalidChars(value) {
+		return "", custom_errors.InvalidCharacters()
 	}
 	return value, nil
 }
