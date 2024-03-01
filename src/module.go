5f196f9086db78449ecef83eb0153405a7a38576
Joao Crulhas <joao.crulhas@gmail.com>
2024-03-01 15:05:40 +0100

    Add encrypter in the DI

--- src/module.go
+++ src/module.go
@@ -6,18 +6,31 @@ import (
 	"github.com/joaoCrulhas/omnevo-super-heroes/src/domain"
 	"github.com/joaoCrulhas/omnevo-super-heroes/src/infra/db"
 	memory_db "github.com/joaoCrulhas/omnevo-super-heroes/src/infra/db/memory"
+	encrypter "github.com/joaoCrulhas/omnevo-super-heroes/src/infra/encrypter/deesee-chiffre"
+	encrypter_validators "github.com/joaoCrulhas/omnevo-super-heroes/src/infra/encrypter/deesee-chiffre/validators"
 	"github.com/joaoCrulhas/omnevo-super-heroes/src/usecases"
 )
 
 // Module Basic struct
 type Module struct{}
 
+const (
+	MinShiftValue = 96
+	MaxShiftValue = 122
+	key           = 5
+)
+
 // Configure DI
 func (m *Module) Configure(injector *dingo.Injector) {
-	memoryDb := memory_db.NewSuperHeroMemoryRepository(nil)
-	superHeroUseCases := usecases.NewSuperHeroUseCase(memoryDb)
+	memoryDb, err := memory_db.NewSuperHeroMemoryRepository(nil)
+	if err != nil {
+		panic(err)
+	}
+	encrypter := encrypter.NewEncryptDeeSeeChiffreService(key, MinShiftValue, MaxShiftValue, encrypter_validators.ValidateEmptyInput, encrypter_validators.ValidateSpecialCharacters)
+	superHeroUseCases := usecases.NewSuperHeroUseCase(memoryDb, encrypter)
 
 	web.BindRoutes(injector, new(Routes))
 	injector.Bind(new(db.Repository[domain.Superhero])).ToInstance(memoryDb)
 	injector.Bind(new(domain.SuperHeroUseCase)).ToInstance(superHeroUseCases)
+	injector.Bind(new(domain.Encrypter)).ToInstance(encrypter)
 }
