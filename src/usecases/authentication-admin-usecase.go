4b84630612fabf554baf0bd5e51a445329f9ca96
Joao Crulhas <joao.crulhas@gmail.com>
2024-03-03 18:27:14 +0100

    Use dingo to handle the config

--- src/usecases/authentication-admin-usecase.go
+++ src/usecases/authentication-admin-usecase.go
@@ -5,10 +5,22 @@ import (
 )
 
 type AuthenticationAdmin struct {
+	apiKey string
 }
 
 func NewAuthenticationAdmin() *AuthenticationAdmin {
 	return &AuthenticationAdmin{}
+}
+
+func (c *AuthenticationAdmin) Inject(
+	cfg *struct {
+		APIKey string `inject:"config:deesee.apikey"`
+	},
+) *AuthenticationAdmin {
+	if cfg != nil {
+		c.apiKey = cfg.APIKey
+	}
+	return c
 }
 
 func (a *AuthenticationAdmin) Auth(headers map[string][]string) (bool, error) {
@@ -16,7 +28,7 @@ func (a *AuthenticationAdmin) Auth(headers map[string][]string) (bool, error) {
 		return false, errors.Unauthorized("Unauthorized")
 	}
 
-	if headers["X-Dee-See-Admin-Key"][0] != "myadminkey" {
+	if headers["X-Dee-See-Admin-Key"][0] != a.apiKey {
 		return false, errors.Unauthorized("Unauthorized")
 	}
 
